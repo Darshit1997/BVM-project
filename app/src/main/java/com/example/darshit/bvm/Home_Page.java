e05948c5382a9c615718d4ff35898e01a968a4f9
Darshit <rupapara.darshit.1@gmail.com>
2018-09-22 12:52:40 +0530

    nothing

--- app/src/main/java/com/example/darshit/bvm/Home_Page.java
+++ app/src/main/java/com/example/darshit/bvm/Home_Page.java
@@ -3,6 +3,9 @@ package com.example.darshit.bvm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class Home_Page extends AppCompatActivity {
 
@@ -12,6 +15,18 @@ public class Home_Page extends AppCompatActivity {
         setContentView(R.layout.activity_home__page);
 
 
+        Toolbar toolbar =  (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
 
+        Button login = (Button) findViewById(R.id.btn_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home_Page.this,LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
